10b75c3c5bed092fcc4f17e0a9810a5b7c004d24
DarianTr <dlt7@unamail.net>
2023-12-06 08:57:07 +0100

    day 6 finished

--- day6/second.zig
+++ day6/second.zig
@@ -12,7 +12,7 @@ fn number_of_ways(time: usize, dist: usize) usize {
 }
 
 pub fn main() !void {
-    const fileName = "first.txt";
+    const fileName = "second.txt";
     const file = try std.fs.cwd().openFile(fileName, .{});
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -32,13 +32,35 @@ pub fn main() !void {
     var it_nt = std.mem.tokenizeAny(u8, nums_t, " ");
     var it_nd = std.mem.tokenizeAny(u8, nums_d, " ");
 
-    var total: usize = 1;
+    const total: usize = 1;
+    _ = total;
+    var num: usize = 0;
+    var dis: usize = 0;
     while (it_nt.next()) |num_t| {
         //std.debug.print("{s} ", .{num_t});
         const n = std.fmt.parseInt(usize, num_t, 10) catch continue;
         const d = std.fmt.parseInt(usize, it_nd.next().?, 10) catch continue;
-        const count = number_of_ways(n, d);
-        total *= count;
+        var digits_d: usize = 1;
+        var digits_n: usize = 1;
+        var n_copy: usize = n;
+        var d_copy: usize = d;
+        while (n_copy / 10 != 0) {
+            digits_n += 1;
+            n_copy /= 10;
+        }
+        while (d_copy / 10 != 0) {
+            digits_d += 1;
+            d_copy /= 10;
+        }
+        dis *= std.math.pow(usize, 10, digits_d);
+        dis += d;
+        num *= std.math.pow(usize, 10, digits_n);
+        num += n;
+        //std.debug.print("{} {} {} {} {} {}\n", .{ dis, num, d, n, digits_d, digits_n });
+
+        //const count = number_of_ways(n, d);
+        //total *= count;
     }
-    std.debug.print("{}\n", .{total});
+    //std.debug.print("{} {}\n", .{ num, dis });
+    std.debug.print("{}\n", .{number_of_ways(num, dis)});
 }
