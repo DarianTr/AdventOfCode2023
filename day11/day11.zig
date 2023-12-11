0b58145930df5256dd513e839df206333e7bed70
DarianTr <dlt7@unamail.net>
2023-12-11 14:54:19 +0100

    day11 part 2 done with prefix sum

--- day11/day11.zig
+++ day11/day11.zig
@@ -20,27 +20,33 @@ fn get_points(map: std.ArrayList(std.ArrayList(u8))) !std.ArrayList(Point) {
     return points;
 }
 
-fn get_distance(p1: Point, p2: Point, xs: std.AutoHashMap(usize, void), ys: std.AutoHashMap(usize, void)) usize {
-    const exp = 10;
-    var x_dif: usize = 0;
-    for (@min(p1.x, p2.x) + 1..@max(p1.x, p2.x) + 1) |x| {
-        if (xs.contains(x)) {
-            x_dif += exp;
-        } else {
-            x_dif += 1;
-        }
-    }
-    var y_dif: usize = 0;
-    for (@min(p1.y, p2.y) + 1..@max(p1.y, p2.y) + 1) |y| {
-        if (ys.contains(y)) {
-            y_dif += exp;
-        } else {
-            y_dif += 1;
-        }
-    }
-    const dist = y_dif + x_dif;
-    return dist;
+fn get_distance(p1: Point, p2: Point, pref_x: std.ArrayList(usize), pref_y: std.ArrayList(usize)) usize {
+    const x = pref_x.items[@max(p1.x, p2.x)] - pref_x.items[@min(p1.x, p2.x)];
+    const y = pref_y.items[@max(p1.y, p2.y)] - pref_y.items[@min(p1.y, p2.y)];
+    return x + y;
 }
+
+// fn get_distance(p1: Point, p2: Point, xs: std.AutoHashMap(usize, void), ys: std.AutoHashMap(usize, void)) usize {
+//     const exp = 2;
+//     var x_dif: usize = 0;
+//     for (@min(p1.x, p2.x) + 1..@max(p1.x, p2.x) + 1) |x| {
+//         if (xs.contains(x)) {
+//             x_dif += exp;
+//         } else {
+//             x_dif += 1;
+//         }
+//     }
+//     var y_dif: usize = 0;
+//     for (@min(p1.y, p2.y) + 1..@max(p1.y, p2.y) + 1) |y| {
+//         if (ys.contains(y)) {
+//             y_dif += exp;
+//         } else {
+//             y_dif += 1;
+//         }
+//     }
+//     const dist = y_dif + x_dif;
+//     return dist;
+// }
 
 fn print_map(map: std.ArrayList(std.ArrayList(u8))) void {
     for (map.items) |row| {
@@ -85,7 +91,7 @@ fn empty_col(map: std.ArrayList(std.ArrayList(u8)), i: usize) bool {
 }
 
 pub fn main() !void {
-    const fileName = "test.txt";
+    const fileName = "puzzle.txt";
     const file = try std.fs.cwd().openFile(fileName, .{});
     const read_buf = try file.readToEndAlloc(allocator, 1024 * 1024);
     var input = std.ArrayList(std.ArrayList(u8)).init(allocator);
@@ -93,7 +99,12 @@ pub fn main() !void {
     defer allocator.free(read_buf);
     defer file.close();
     const empty_xs = std.AutoHashMap(usize, void).init(allocator);
+    _ = empty_xs;
     var empty_ys = std.AutoHashMap(usize, void).init(allocator);
+    var prefsum_x = std.ArrayList(usize).init(allocator);
+    var prefsum_y = std.ArrayList(usize).init(allocator);
+    try prefsum_y.append(0);
+    try prefsum_x.append(0);
     while (line_it.next()) |line| {
         var line_arr = std.ArrayList(u8).init(allocator);
         for (0..line.len) |i| {
@@ -101,40 +112,32 @@ pub fn main() !void {
         }
         try input.append(line_arr);
     }
-    var i: usize = 0;
-    for (input.items) |_| { //watch out input.items.len changes
+    const exp = 1_000_000;
+    for (0..input.items.len) |i| {
         if (empty_row(input, i)) {
-            // try insert_row(&input, i);
-            // i += 1;
+            try prefsum_y.append(prefsum_y.getLast() + exp);
+
             try empty_ys.put(i, {});
+        } else {
+            try prefsum_y.append(prefsum_y.getLast() + 1);
         }
-        i += 1;
     }
-    //std.debug.print("\n", .{});
-    i = 0;
-    for (0..input.getLast().items.len) |_| {
+    for (0..input.getLast().items.len) |i| {
         if (empty_col(input, i)) {
-            // try insert_col(&input, i);
-            // i += 1;
+            try prefsum_x.append(prefsum_x.getLast() + exp);
+
             try empty_ys.put(i, {});
+        } else {
+            try prefsum_x.append(prefsum_x.getLast() + 1);
         }
-        i += 1;
     }
-    //print_map(input);
     const points = try get_points(input);
     var total: usize = 0;
     for (0..points.items.len) |outer| {
         for (outer..points.items.len) |j| {
-            const dist = get_distance(points.items[outer], points.items[j], empty_xs, empty_ys);
+            const dist = get_distance(points.items[outer], points.items[j], prefsum_x, prefsum_y);
             total += dist;
-            //std.debug.print("{} {}\n", .{ points.items.len, dist });
         }
     }
     std.debug.print("{}\n", .{total});
 }
-
-//for part two: im array speichern, ob die x koordinate vermillionfacht wird oder nicht.
-// das gleiche auch f"ur y koords. Dann bei der dist func uber alle x un y in dem array fuer den bestimmen bereich gehen
-// und entweder 1 oder 1M dazuaddiern. Arrays sollten of type bool sein
-
-//1084 statt 1030
