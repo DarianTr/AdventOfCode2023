289ad72e8cc78487ae725217970fa5d636ae4beb
DarianTr <dlt7@unamail.net>
2023-12-11 14:37:09 +0100

    switch to pc

--- day11/day11.zig
+++ day11/day11.zig
@@ -20,14 +20,23 @@ fn get_points(map: std.ArrayList(std.ArrayList(u8))) !std.ArrayList(Point) {
     return points;
 }
 
-fn get_distance(p1: Point, p2: Point) i128 {
-    var x_dif = (@as(i128, p1.x) - p2.x);
-    var y_dif = (@as(i128, p1.y) - p2.y);
-    if (x_dif < 0) {
-        x_dif *= -1;
+fn get_distance(p1: Point, p2: Point, xs: std.AutoHashMap(usize, void), ys: std.AutoHashMap(usize, void)) usize {
+    const exp = 10;
+    var x_dif: usize = 0;
+    for (@min(p1.x, p2.x) + 1..@max(p1.x, p2.x) + 1) |x| {
+        if (xs.contains(x)) {
+            x_dif += exp;
+        } else {
+            x_dif += 1;
+        }
     }
-    if (y_dif < 0) {
-        y_dif *= -1;
+    var y_dif: usize = 0;
+    for (@min(p1.y, p2.y) + 1..@max(p1.y, p2.y) + 1) |y| {
+        if (ys.contains(y)) {
+            y_dif += exp;
+        } else {
+            y_dif += 1;
+        }
     }
     const dist = y_dif + x_dif;
     return dist;
@@ -76,13 +85,15 @@ fn empty_col(map: std.ArrayList(std.ArrayList(u8)), i: usize) bool {
 }
 
 pub fn main() !void {
-    const fileName = "puzzle.txt";
+    const fileName = "test.txt";
     const file = try std.fs.cwd().openFile(fileName, .{});
     const read_buf = try file.readToEndAlloc(allocator, 1024 * 1024);
     var input = std.ArrayList(std.ArrayList(u8)).init(allocator);
     var line_it = std.mem.split(u8, read_buf, "\n");
     defer allocator.free(read_buf);
     defer file.close();
+    const empty_xs = std.AutoHashMap(usize, void).init(allocator);
+    var empty_ys = std.AutoHashMap(usize, void).init(allocator);
     while (line_it.next()) |line| {
         var line_arr = std.ArrayList(u8).init(allocator);
         for (0..line.len) |i| {
@@ -93,8 +104,9 @@ pub fn main() !void {
     var i: usize = 0;
     for (input.items) |_| { //watch out input.items.len changes
         if (empty_row(input, i)) {
-            try insert_row(&input, i);
-            i += 1;
+            // try insert_row(&input, i);
+            // i += 1;
+            try empty_ys.put(i, {});
         }
         i += 1;
     }
@@ -102,17 +114,18 @@ pub fn main() !void {
     i = 0;
     for (0..input.getLast().items.len) |_| {
         if (empty_col(input, i)) {
-            try insert_col(&input, i);
-            i += 1;
+            // try insert_col(&input, i);
+            // i += 1;
+            try empty_ys.put(i, {});
         }
         i += 1;
     }
     //print_map(input);
     const points = try get_points(input);
-    var total: i128 = 0;
+    var total: usize = 0;
     for (0..points.items.len) |outer| {
         for (outer..points.items.len) |j| {
-            const dist = get_distance(points.items[outer], points.items[j]);
+            const dist = get_distance(points.items[outer], points.items[j], empty_xs, empty_ys);
             total += dist;
             //std.debug.print("{} {}\n", .{ points.items.len, dist });
         }
@@ -123,3 +136,5 @@ pub fn main() !void {
 //for part two: im array speichern, ob die x koordinate vermillionfacht wird oder nicht.
 // das gleiche auch f"ur y koords. Dann bei der dist func uber alle x un y in dem array fuer den bestimmen bereich gehen
 // und entweder 1 oder 1M dazuaddiern. Arrays sollten of type bool sein
+
+//1084 statt 1030
