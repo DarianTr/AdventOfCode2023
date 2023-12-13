eb60538bc12cc4c3ea2c5199eaffc56e31a3ffa2
DarianTr <dlt7@unamail.net>
2023-12-13 23:20:19 +0100

    day 13 part 2

--- day13/day13.zig
+++ day13/day13.zig
@@ -3,33 +3,35 @@ const std = @import("std");
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 const allocator = arena.allocator();
 
-fn is_mirror_col(left: usize, right: usize, map: std.ArrayList([]const u8)) bool {
+fn is_mirror_col(left: usize, right: usize, map: std.ArrayList([]const u8)) usize {
+    var diff: usize = 0;
     var l = left;
     var r = right;
     while (l >= 0 and r < map.getLast().len) {
         for (0..map.items.len) |i| {
             if (map.items[i][l] != map.items[i][r]) {
-                return false;
+                diff += 1;
             }
         }
 
         if (l == 0) {
-            return true;
+            return diff;
         }
 
         l -= 1;
         r += 1;
     }
-    return true;
+    return diff;
 }
 
-fn is_mirror_row(under: usize, above: usize, map: std.ArrayList([]const u8)) bool {
+fn is_mirror_row(under: usize, above: usize, map: std.ArrayList([]const u8)) usize {
     var u = under;
     var a = above;
+    var diff: usize = 0;
     while (a >= 0 and u < map.items.len) {
         for (0..map.getLast().len) |i| {
             if (map.items[a][i] != map.items[u][i]) {
-                return false;
+                diff += 1;
             }
         }
         // if (!std.mem.eql(u8, map.items[u], map.items[a])) {
@@ -39,12 +41,12 @@ fn is_mirror_row(under: usize, above: usize, map: std.ArrayList([]const u8)) boo
 
         u += 1;
         if (a == 0) {
-            return true;
+            return diff;
         }
         a -= 1;
     }
 
-    return true;
+    return diff;
 }
 
 pub fn main() !void {
@@ -57,21 +59,30 @@ pub fn main() !void {
 
     var it = std.mem.split(u8, read_buf, "\n\n");
 
-    while (it.next()) |b| {
+    outer: while (it.next()) |b| {
         var map = std.ArrayList([]const u8).init(allocator);
         var it_line = std.mem.split(u8, b, "\n");
         while (it_line.next()) |l| {
             try map.append(l);
         }
-
-        for (0..map.items.len - 1) |i| {
-            if (is_mirror_row(i + 1, i, map)) {
+        var i: usize = map.items.len - 2;
+        while (true) : (i -= 1) {
+            if (is_mirror_row(i + 1, i, map) == 1) {
                 row += i + 1;
+                continue :outer;
+            }
+            if (i == 0) {
+                break;
             }
         }
-        for (0..map.getLast().len - 1) |i| {
-            if (is_mirror_col(i, i + 1, map)) {
+        i = map.getLast().len - 2;
+        while (true) : (i -= 1) {
+            if (is_mirror_col(i, i + 1, map) == 1) {
                 col += i + 1;
+                continue :outer;
+            }
+            if (i == 0) {
+                break;
             }
         }
     }
