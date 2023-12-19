d16c675b2b20dd17f3db53280b19cb21195c7ce5
DarianTr <dlt7@unamail.net>
2023-12-19 22:44:18 +0100

    change to windows

--- day19/day19.go
+++ day19/day19.go
@@ -6,9 +6,28 @@ import (
 	"os"
 )
 
+type Val struct {
+	a int
+	m int
+	s int
+	x int
+}
+
+type FPtr struct {
+	cmp func (Val) bool
+	res string
+}
+
+type Workflow struct {
+	name string
+	functions []FPtr
+}
+
+
+func get_next(v Val, w Workflow, m map[string]Workflow)
+
 func main() {
-	dat, _ := os.ReadFile("puzzle.txt")
+	dat, _ := os.ReadFile("test.txt")
 	split := bytes.Split(dat, []byte("\n"))
-	ans := cycles([100][]byte(split))
-	fmt.Println("Ans:", ans)
+	fmt.Println("Ans:", 1)
 }
